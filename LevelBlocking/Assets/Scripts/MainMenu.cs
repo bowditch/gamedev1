88a5f7719f3567e81ac375f32561f1a89e5d86ca
John Bowditch <john.bowditch@me.com>
2021-03-24 11:23:02 -0400

    Fixed reference errors

--- LevelBlocking/Assets/Scripts/MainMenu.cs
+++ LevelBlocking/Assets/Scripts/MainMenu.cs
@@ -6,9 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-public string StartExperience;
   public void StartExperience()
-  
   {
 
     SceneManager.LoadScene("SecondFloor");
